03f38c7a2ac99268071672cc41876c2b22bdbb5d
Hong <hjihun2315@gmail.com>
2021-06-09 18:50:13 +0900

    일단 돌아가는 코드 - 가중치 업데이트가 안됨 ㅋㅋ

--- alex_net.py
+++ alex_net.py
@@ -11,7 +11,7 @@ import numpy as np
 import random
 
 RANDOM_SEED = 602
-random.seed(RANDOM_SEED)
+# random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
 
 # Hyper-parameters
@@ -69,8 +69,8 @@ def load_dataset(path):
     # image = tf.image.crop_and_resize(image, crop_size=(227,227), boxes=[900, 4])
 
     # Shuffle with seed can keep the data-label pair. Without shuffle, data have same label in range.
-    random.shuffle(images)
-    random.shuffle(labels)
+    random.Random(RANDOM_SEED).shuffle(images)
+    random.Random(RANDOM_SEED).shuffle(labels)
 
     # Split train/valid/test, total data size = 5,000
     train_X, train_Y = images[:int(len(images)*0.8)], labels[:int(len(labels)*0.8)]
@@ -117,80 +117,79 @@ def load_dataset(path):
 # self.w8 = tf.Variable(tf.random.normal(shape=[4096, NUM_CLASSES], mean=0.0, stddev=0.01, dtype=tf.float32), name='w8', trainable=True)
 # self.b8 = tf.Variable(tf.zeros(shape=[NUM_CLASSES]), trainable=True, name='b8')
 
-def loss(step, x, y):
+def loss(step, x, y, param):
     inputs = tf.constant(x, name = 'inputs')
 
     # layer 1
-    w1 = tf.Variable(tf.random.normal(shape=[11,11,3,96], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w1')
-    b1 = tf.Variable(tf.zeros(shape=[96]), trainable=True, name='b1')
+    # w1 = tf.Variable(tf.random.normal(shape=[11,11,3,96], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w1')
+    # b1 = tf.Variable(tf.zeros(shape=[96]), trainable=True, name='b1')
 
-    l1_convolve = tf.nn.conv2d(input=inputs, filters=w1, strides=4, padding='VALID', name='l1_convolve')
-    l1_bias = tf.reshape(tf.nn.bias_add(l1_convolve, b1), tf.shape(l1_convolve), name='l1_bias')
+    l1_convolve = tf.nn.conv2d(input=inputs, filters=param['w1'], strides=4, padding='VALID', name='l1_convolve')
+    l1_bias = tf.reshape(tf.nn.bias_add(l1_convolve, param['b1']), tf.shape(l1_convolve), name='l1_bias')
     l1_relu = tf.nn.relu(l1_bias, name='l1_relu')
     l1_norm = tf.nn.lrn(input=l1_relu, depth_radius=5, alpha=10e-4, beta=0.75, bias=2.0, name='l1_norm')
     l1_pool = tf.nn.max_pool(input=l1_norm, ksize=3, strides=2, padding='VALID', name='l1_pool')
 
     # layer 2
-    w2 = tf.Variable(tf.random.normal(shape=[5, 5, 96, 256], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w2')
-    b2 = tf.Variable(tf.ones(shape=[256]), trainable=True, name='b2')
-    l2_convolve = tf.nn.conv2d(input=l1_pool, filters=w2, strides=1, padding='SAME', name='l2_convolve')
-    l2_bias = tf.reshape(tf.nn.bias_add(l2_convolve, b2), tf.shape(l2_convolve), name='l2_bias')
+    # w2 = tf.Variable(tf.random.normal(shape=[5, 5, 96, 256], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w2')
+    # b2 = tf.Variable(tf.ones(shape=[256]), trainable=True, name='b2')
+    l2_convolve = tf.nn.conv2d(input=l1_pool, filters=param['w2'], strides=1, padding='SAME', name='l2_convolve')
+    l2_bias = tf.reshape(tf.nn.bias_add(l2_convolve, param['b2']), tf.shape(l2_convolve), name='l2_bias')
     l2_relu = tf.nn.relu(l2_bias, name='l2_relu')
     l2_norm = tf.nn.lrn(input=l2_relu, depth_radius=5, alpha=10e-4, beta=0.75, bias=2.0, name='l2_norm')
     l2_pool = tf.nn.max_pool(input=l2_norm, ksize=3, strides=2, padding='VALID', name='l2_pool')
 
     # layer 3
-    w3 = tf.Variable(tf.random.normal(shape=[3, 3, 256, 384], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w3')
-    b3 = tf.Variable(tf.zeros(shape=[384]), trainable=True, name='b3')
-    l3_convolve = tf.nn.conv2d(input=l2_pool, filters=w3, strides=1, padding='SAME', name='l3_convolve')
-    l3_bias = tf.reshape(tf.nn.bias_add(l3_convolve, b3), tf.shape(l3_convolve), name='l3_bias')
+    # w3 = tf.Variable(tf.random.normal(shape=[3, 3, 256, 384], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w3')
+    # b3 = tf.Variable(tf.zeros(shape=[384]), trainable=True, name='b3')
+    l3_convolve = tf.nn.conv2d(input=l2_pool, filters=param['w3'], strides=1, padding='SAME', name='l3_convolve')
+    l3_bias = tf.reshape(tf.nn.bias_add(l3_convolve, param['b3']), tf.shape(l3_convolve), name='l3_bias')
     l3_relu = tf.nn.relu(l3_bias, name='l3_relu')
 
     # layer 4
-    w4 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 384], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w4')
-    b4 = tf.Variable(tf.ones(shape=[384]), trainable=True, name='b4')
-    l4_convolve = tf.nn.conv2d(input=l3_relu, filters=w4, strides=1, padding='SAME', name='l4_convolve')
-    l4_bias = tf.reshape(tf.nn.bias_add(l4_convolve, b4), tf.shape(l4_convolve), name='l4_bias')
+    # w4 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 384], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w4')
+    # b4 = tf.Variable(tf.ones(shape=[384]), trainable=True, name='b4')
+    l4_convolve = tf.nn.conv2d(input=l3_relu, filters=param['w4'], strides=1, padding='SAME', name='l4_convolve')
+    l4_bias = tf.reshape(tf.nn.bias_add(l4_convolve, param['b4']), tf.shape(l4_convolve), name='l4_bias')
     l4_relu = tf.nn.relu(l4_bias, name='l4_relu')
 
     # layer 5
-    w5 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 256], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w5')
-    b5 = tf.Variable(tf.ones(shape=[256]), trainable=True, name='b5')
-    l5_convolve = tf.nn.conv2d(input=l4_relu, filters=w5, strides=1, padding='SAME', name='l5_convolve')
-    l5_bias = tf.reshape(tf.nn.bias_add(l5_convolve, b5), tf.shape(l5_convolve), name='l5_bias')
+    # w5 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 256], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w5')
+    # b5 = tf.Variable(tf.ones(shape=[256]), trainable=True, name='b5')
+    l5_convolve = tf.nn.conv2d(input=l4_relu, filters=param['w5'], strides=1, padding='SAME', name='l5_convolve')
+    l5_bias = tf.reshape(tf.nn.bias_add(l5_convolve, param['b5']), tf.shape(l5_convolve), name='l5_bias')
     l5_relu = tf.nn.relu(l5_bias, name='l5_relu')
     l5_pool = tf.nn.max_pool(input=l5_relu, ksize=3, strides=2, padding='VALID', name='l5_pool')
 
     # layer 6
-    w6 = tf.Variable(tf.random.normal(shape=[6 * 6 * 256, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w6')
-    b6 = tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b6')
-    l6_flattened = tf.reshape(l5_pool, [-1, tf.shape(w6)[0]], name='l6_flattened')
-    l6_fc = tf.nn.bias_add(tf.matmul(l6_flattened, w6), b6, name='l6_fc')
+    # w6 = tf.Variable(tf.random.normal(shape=[6 * 6 * 256, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w6')
+    # b6 = tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b6')
+    l6_flattened = tf.reshape(l5_pool, [-1, tf.shape(param['w6'])[0]], name='l6_flattened')
+    l6_fc = tf.nn.bias_add(tf.matmul(l6_flattened, param['w6']), param['b6'], name='l6_fc')
     l6_relu = tf.nn.relu(l6_fc, name='l6_relu')
     l6_dropout = tf.nn.dropout(l6_relu, rate=0.5, name='l6_dropout')
 
     # layer 7
-    w7 = tf.Variable(tf.random.normal(shape=[4096, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), name='w7', trainable=True)
-    b7 = tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b7')
-    l7_fc = tf.nn.bias_add(tf.matmul(l6_dropout, w7), b7, name='l7_fc')
+    # w7 = tf.Variable(tf.random.normal(shape=[4096, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), name='w7', trainable=True)
+    # b7 = tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b7')
+    l7_fc = tf.nn.bias_add(tf.matmul(l6_dropout, param['w7']), param['b7'], name='l7_fc')
     l7_relu = tf.nn.relu(l7_fc, name='l7_relu')
     l7_dropout = tf.nn.dropout(l7_relu, rate=0.5, name='l7_dropout')
 
     # layer 8
-    w8 = tf.Variable(tf.random.normal(shape=[4096, NUM_CLASSES], mean=0.0, stddev=0.01, dtype=tf.float32), name='w8', trainable=True)
-    b8 = tf.Variable(tf.zeros(shape=[NUM_CLASSES]), trainable=True, name='b8')
-    l8_fc = tf.nn.bias_add(tf.matmul(l7_dropout, w8), b8, name='l8_fc')
+    # w8 = tf.Variable(tf.random.normal(shape=[4096, NUM_CLASSES], mean=0.0, stddev=0.01, dtype=tf.float32), name='w8', trainable=True)
+    # b8 = tf.Variable(tf.zeros(shape=[NUM_CLASSES]), trainable=True, name='b8')
+    l8_fc = tf.nn.bias_add(tf.matmul(l7_dropout, param['w8']), param['b8'], name='l8_fc')
     logits = tf.nn.softmax(l8_fc, axis=1, name='l8_softmax')
+    predict = tf.argmax(logits, 1).numpy()
 
-    loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
     loss = tf.reduce_mean(loss)
 
-    if step % 100 == 0:
-        predict = tf.argmax(logits, 1).numpy()
-        target = np.argmax(y, 1)
-        accuracy = np.sum(predict == target) / len(target)
+    target = y
+    accuracy = np.sum(predict == target) / len(target)
 
-        print('Step {}:\tloss = {}\taccuracy = {}'.format(step, loss.numpy(), accuracy))
+    print('epoch {}:\tloss = {}\taccuracy = {}'.format(step, loss.numpy(), accuracy))
 
     return loss
 
@@ -201,13 +200,39 @@ def loss(step, x, y):
 
 # # Create optimizer and apply gradient descent to the trainable variables
 optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=LR_INIT, weight_decay=LR_DECAY, name='optimizer')
-# optimizer = tf.optimizers.SGD(momentum=MOMENTUM, learning_rate=LR_INIT, name='optimizer')
-var_list = {'w1',}
-for epoch in range(NUM_EPOCHS):
-    train_X, train_Y, valid_X, valid_Y, test_X, test_Y = load_dataset(TRAIN_IMG_DIR)
+parameters = {
+    'w1': tf.Variable(tf.random.normal(shape=[11, 11, 3, 96], mean=0.0, stddev=0.01, dtype=tf.float32), name='w1', trainable=True),
+    'b1': tf.Variable(tf.zeros(shape=[96]), trainable=True, name='b1'),
 
+    'w2': tf.Variable(tf.random.normal(shape=[5, 5, 96, 256], mean=0.0, stddev=0.01, dtype=tf.float32), name='w2', trainable=True),
+    'b2': tf.Variable(tf.ones(shape=[256]), trainable=True, name='b2'),
+
+    'w3': tf.Variable(tf.random.normal(shape=[3, 3, 256, 384], mean=0.0, stddev=0.01, dtype=tf.float32), name='w3', trainable=True),
+    'b3': tf.Variable(tf.zeros(shape=[384]), trainable=True, name='b3'),
+
+    'w4': tf.Variable(tf.random.normal(shape=[3, 3, 384, 384], mean=0.0, stddev=0.01, dtype=tf.float32), name='w4', trainable=True),
+    'b4': tf.Variable(tf.ones(shape=[384]), trainable=True, name='b4'),
+
+    'w5': tf.Variable(tf.random.normal(shape=[3, 3, 384, 256], mean=0.0, stddev=0.01, dtype=tf.float32), name='w5', trainable=True),
+    'b5': tf.Variable(tf.ones(shape=[256]), trainable=True, name='b5'),
+
+    'w6': tf.Variable(tf.random.normal(shape=[6*6*256, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), name='w6', trainable=True),
+    'b6': tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b6'),
+
+    'w7': tf.Variable(tf.random.normal(shape=[4096, 4096], mean=0.0, stddev=0.01, dtype=tf.float32), name='w7', trainable=True),
+    'b7': tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b7'),
+
+    'w8': tf.Variable(tf.random.normal(shape=[4096, NUM_CLASSES], mean=0.0, stddev=0.01, dtype=tf.float32), name='w8', trainable=True),
+    'b8': tf.Variable(tf.zeros(shape=[NUM_CLASSES]), trainable=True, name='b8'),
+}
+# var_list = {'w1',}
+train_X, train_Y, valid_X, valid_Y, test_X, test_Y = load_dataset(TRAIN_IMG_DIR)
+for epoch in range(NUM_EPOCHS):
+    foo = 1
     for batch_X, batch_Y in zip(list(train_X.as_numpy_iterator()), list(train_Y.as_numpy_iterator())):
-        optimizer.minimize(lambda: loss(epoch, batch_X, batch_Y), var_list=[])
+        print('batch {}'.format(foo))
+        foo += 1
+        optimizer.minimize(lambda: loss(epoch, batch_X, batch_Y, parameters), var_list=parameters)
 
 # Define training loop
 ########################################################################################################################

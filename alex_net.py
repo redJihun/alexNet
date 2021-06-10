06d932ead92d15982a48bc056dcd7faf267e6cd6
Hong <hjihun2315@gmail.com>
2021-06-10 18:53:33 +0900

    add model save & load, add validation step

--- alex_net.py
+++ alex_net.py
@@ -1,5 +1,6 @@
 # For files
 import os
+import time
 
 # Deep-learning framework
 import tensorflow as tf
@@ -69,8 +70,11 @@ def load_dataset(path):
     # image = tf.image.crop_and_resize(image, crop_size=(227,227), boxes=[900, 4])
 
     # Shuffle with seed can keep the data-label pair. Without shuffle, data have same label in range.
-    random.Random(RANDOM_SEED).shuffle(images)
-    random.Random(RANDOM_SEED).shuffle(labels)
+    foo = list(zip(images, labels))
+    random.Random(RANDOM_SEED).shuffle(foo)
+    # random.Random(RANDOM_SEED).shuffle(images)
+    # random.Random(RANDOM_SEED).shuffle(labels)
+    images, labels = zip(*foo)
 
     # Split train/valid/test, total data size = 5,000
     train_X, train_Y = images[:int(len(images)*0.8)], labels[:int(len(labels)*0.8)]
@@ -123,10 +127,10 @@ def loss(step, x, y, param):
     # layer 1
     # w1 = tf.Variable(tf.random.normal(shape=[11,11,3,96], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w1')
     # b1 = tf.Variable(tf.zeros(shape=[96]), trainable=True, name='b1')
-
     l1_convolve = tf.nn.conv2d(input=inputs, filters=param['w1'], strides=4, padding='VALID', name='l1_convolve')
     l1_bias = tf.reshape(tf.nn.bias_add(l1_convolve, param['b1']), tf.shape(l1_convolve), name='l1_bias')
     l1_relu = tf.nn.relu(l1_bias, name='l1_relu')
+    # l1_relu = tf.nn.leaky_relu(l1_bias, name='l1_relu')
     l1_norm = tf.nn.lrn(input=l1_relu, depth_radius=5, alpha=10e-4, beta=0.75, bias=2.0, name='l1_norm')
     l1_pool = tf.nn.max_pool(input=l1_norm, ksize=3, strides=2, padding='VALID', name='l1_pool')
 
@@ -136,6 +140,7 @@ def loss(step, x, y, param):
     l2_convolve = tf.nn.conv2d(input=l1_pool, filters=param['w2'], strides=1, padding='SAME', name='l2_convolve')
     l2_bias = tf.reshape(tf.nn.bias_add(l2_convolve, param['b2']), tf.shape(l2_convolve), name='l2_bias')
     l2_relu = tf.nn.relu(l2_bias, name='l2_relu')
+    # l2_relu = tf.nn.leaky_relu(l2_bias, name='l2_relu')
     l2_norm = tf.nn.lrn(input=l2_relu, depth_radius=5, alpha=10e-4, beta=0.75, bias=2.0, name='l2_norm')
     l2_pool = tf.nn.max_pool(input=l2_norm, ksize=3, strides=2, padding='VALID', name='l2_pool')
 
@@ -145,6 +150,7 @@ def loss(step, x, y, param):
     l3_convolve = tf.nn.conv2d(input=l2_pool, filters=param['w3'], strides=1, padding='SAME', name='l3_convolve')
     l3_bias = tf.reshape(tf.nn.bias_add(l3_convolve, param['b3']), tf.shape(l3_convolve), name='l3_bias')
     l3_relu = tf.nn.relu(l3_bias, name='l3_relu')
+    # l3_relu = tf.nn.leaky_relu(l3_bias, name='l3_relu')
 
     # layer 4
     # w4 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 384], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w4')
@@ -152,6 +158,7 @@ def loss(step, x, y, param):
     l4_convolve = tf.nn.conv2d(input=l3_relu, filters=param['w4'], strides=1, padding='SAME', name='l4_convolve')
     l4_bias = tf.reshape(tf.nn.bias_add(l4_convolve, param['b4']), tf.shape(l4_convolve), name='l4_bias')
     l4_relu = tf.nn.relu(l4_bias, name='l4_relu')
+    # l4_relu = tf.nn.leaky_relu(l4_bias, name='l4_relu')
 
     # layer 5
     # w5 = tf.Variable(tf.random.normal(shape=[3, 3, 384, 256], mean=0.0, stddev=0.01, dtype=tf.float32), trainable=True, name='w5')
@@ -159,6 +166,7 @@ def loss(step, x, y, param):
     l5_convolve = tf.nn.conv2d(input=l4_relu, filters=param['w5'], strides=1, padding='SAME', name='l5_convolve')
     l5_bias = tf.reshape(tf.nn.bias_add(l5_convolve, param['b5']), tf.shape(l5_convolve), name='l5_bias')
     l5_relu = tf.nn.relu(l5_bias, name='l5_relu')
+    # l5_relu = tf.nn.leaky_relu(l5_bias, name='l5_relu')
     l5_pool = tf.nn.max_pool(input=l5_relu, ksize=3, strides=2, padding='VALID', name='l5_pool')
 
     # layer 6
@@ -167,6 +175,7 @@ def loss(step, x, y, param):
     l6_flattened = tf.reshape(l5_pool, [-1, tf.shape(param['w6'])[0]], name='l6_flattened')
     l6_fc = tf.nn.bias_add(tf.matmul(l6_flattened, param['w6']), param['b6'], name='l6_fc')
     l6_relu = tf.nn.relu(l6_fc, name='l6_relu')
+    # l6_relu = tf.nn.leaky_relu(l6_fc, name='l6_relu')
     l6_dropout = tf.nn.dropout(l6_relu, rate=0.5, name='l6_dropout')
 
     # layer 7
@@ -174,18 +183,26 @@ def loss(step, x, y, param):
     # b7 = tf.Variable(tf.ones(shape=[4096]), trainable=True, name='b7')
     l7_fc = tf.nn.bias_add(tf.matmul(l6_dropout, param['w7']), param['b7'], name='l7_fc')
     l7_relu = tf.nn.relu(l7_fc, name='l7_relu')
+    # l7_relu = tf.nn.leaky_relu(l7_fc, name='l7_relu')
     l7_dropout = tf.nn.dropout(l7_relu, rate=0.5, name='l7_dropout')
 
     # layer 8
     # w8 = tf.Variable(tf.random.normal(shape=[4096, NUM_CLASSES], mean=0.0, stddev=0.01, dtype=tf.float32), name='w8', trainable=True)
     # b8 = tf.Variable(tf.zeros(shape=[NUM_CLASSES]), trainable=True, name='b8')
-    l8_fc = tf.nn.bias_add(tf.matmul(l7_dropout, param['w8']), param['b8'], name='l8_fc')
-    logits = tf.nn.softmax(l8_fc, axis=1, name='l8_softmax')
+    # l8_fc = tf.nn.bias_add(tf.matmul(l7_dropout, param['w8']), param['b8'], name='l8_fc')
+    # logits = tf.nn.softmax(l8_fc, axis=1, name='l8_softmax')
+    logits = tf.nn.bias_add(tf.matmul(l7_dropout, param['w8']), param['b8'], name='l8_fc')
     predict = tf.argmax(logits, 1).numpy()
+    # print(logits)
+    # print(np.reshape(y, [len(y), 1]))
+    # tf.reshape(y, shape=(tf.shape(y), 1))
+    # print(y)
 
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    # loss = tf.nn.softmax_cross_entropy_with_logits(labels=np.reshape(y, [len(y), 1]), logits=logits)
+    # loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predict)
+    # loss = tf.square(y - predict)
     loss = tf.reduce_mean(loss)
-
     target = y
     accuracy = np.sum(predict == target) / len(target)
 
@@ -273,7 +290,14 @@ def loss(step, x, y, param):
 # Make CNN model
 
 # # Create optimizer and apply gradient descent to the trainable variables
-optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=LR_INIT, weight_decay=LR_DECAY, name='optimizer')
+
+# lr_schedule = tf.optimizers.schedules.De(
+#     initial_learning_rate= 0.001,
+#     decay_steps=
+# )
+# weight decay
+# optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=0.001, weight_decay=0.5, name='optimizer')
+optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=0.001, weight_decay=LR_DECAY, name='optimizer')
 # optimizer = tf.optimizers.SGD(learning_rate=LR_INIT, name='optimizer')
 parameters = {
     'w1': tf.Variable(tf.random.normal(shape=[11, 11, 3, 96], mean=0.0, stddev=0.01, dtype=tf.float32), name='w1', trainable=True),
@@ -302,15 +326,16 @@ parameters = {
 }
 # var_list = {'w1',}
 train_X, train_Y, valid_X, valid_Y, test_X, test_Y = load_dataset(TRAIN_IMG_DIR)
+# checkpoint = tf.train.Checkpoint(CHECKPOINT_DIR)
+np.savez(os.path.join(CHECKPOINT_DIR, 'trained_parameters'+time.strftime('%y%m%d%H%M%S', time.localtime())), parameters)
 for epoch in range(NUM_EPOCHS):
     foo = 1
     for batch_X, batch_Y in zip(list(train_X.as_numpy_iterator()), list(train_Y.as_numpy_iterator())):
         print('batch {}'.format(foo))
         foo += 1
         optimizer.minimize(lambda: loss(epoch, batch_X, batch_Y, parameters), var_list=parameters)
-        # optimizer.minimize(lambda: loss(epoch, batch_X, batch_Y), var_list=[w1, b1, w2, b2, w3, b3, ])
-        print(parameters['w1'])
 
+np.savez(os.path.join(CHECKPOINT_DIR, 'trained_parameters'+time.strftime('%y%m%d%H%M%S', time.localtime())), parameters)
 # Define training loop
 ########################################################################################################################
 
--- alex_net_with_tape.py
+++ alex_net_with_tape.py
@@ -50,5 +50,22 @@ def load_dataset(path):
     imagepaths, labels = list(), list()
     # 문자열 형태의 라벨을 정수형으로 추가하기 위해 0으로 초기화된 변수 선언, 이후 하위 디렉토리를 순회하면서 1씩 늘어남
     label = 0
-    # 루트 디렉토리 경로로 접근, 하위 디렉토
+    # 루트 디렉토리 경로로 접근, [0] = 루트 디렉토리 경로(PWD), [1] = 모든 하위 디렉토리 리스트, [2] = 루트 디렉토리에 포함된 모든 파일 리스트
     classes = sorted(os.walk(path).__next__()[1])
+    # 하위 디렉토리 순회
+    for c in classes:
+        # join(루트 디렉토리 경로, 현재 순회 중인 서브 디렉토리명)
+        c_dir = os.path.join(path, c)
+        # 서브 디렉토리에 포함된 모든 이미지 파일 불러오기 위한 walk 선언
+        walk = os.walk(c_dir).__next__()
+        # [2] = 포함된 모든 파일
+        for sample in walk[2]:
+            # 이미지 파일경로 리스트 imagepaths, 서브 디렉토리 + 파일이름으로 이미지 파일 절대경로를 리스트에 추가해줌
+            imagepaths.append(os.path.join(c_dir, sample))
+            # 원 라벨명은 서브 디렉토리명이지만, 편의를 위해 정수형으로 추가해줌(oooo1, oooo2, oooo3 -> 1, 2, 3)
+            labels.append(label)
+        # 다음 디렉토리, 라벨 1 추가
+        label += 1
+
+        # 이미지 경로명 리스트의 원소로 접근 -> 이미지읽기(read_file) -> 이미지 디코드 -> 이미지 리사이즈
+        images = list()

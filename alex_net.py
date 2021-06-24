fix: (underfitting occur)edit batch_size, edit optimizer, edit loss func, add scaling -> still underfitting

alex_net.py
@@ -20,7 +20,7 @@ RANDOM_SEED = 602
 
 # Hyper-parameters
 NUM_EPOCHS = 90
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 MOMENTUM = 0.9
 LR_DECAY = 0.0005         # == weight_decay
 LR_INIT = 0.01
@@ -112,7 +112,7 @@ def fancy_pca(images, labels, alpha_std=0.1):
         # R, G, B 채널을 각각 순회하며 계산된 값을 각 픽셀마다 가감
         for idx in range(3):
             orig_img[..., idx] += add_vect[idx]
-            minmax_scale(orig_img[..., idx], feature_range=(0., 1.), copy=False)
+            # minmax_scale(orig_img[..., idx], feature_range=(0., 1.), copy=False)
 
         # 0~255(rgb픽셀값) 범위로 값 재설정
         pca_img = orig_img
@@ -120,6 +120,18 @@ def fancy_pca(images, labels, alpha_std=0.1):
         pca_labels.append(lbl)
     # print('End jittering')
     return pca_images, pca_labels
+
+
+def minmax(images):
+    scaled_images = list()
+    for img in images:
+        # R, G, B 채널을 각각 순회하며 계산된 값을 각 픽셀마다 가감
+        scaled_img = np.array(img).copy()
+        for idx in range(3):
+            scaled_img[..., idx] = minmax_scale(img[..., idx], feature_range=(-1, 1))
+        scaled_images.append(scaled_img)
+
+    return scaled_images
 
 
 # horizontal reflection
@@ -238,8 +250,8 @@ def loss(batch_num, x, y, param, step, epoch):
 
     # 멀티 라벨로 모델 평가 시에 수렴되지 않는 문제 발생(추가적인 연구 필요, 아마 데이터의 클래스 개수가 너무 적어 그런것으로 예상)
     # 단일 라벨로 평가(sparse_softmax_cross_entropy_with_logits)
-    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
-    # loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=predict)
+    # loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=logits)
+    loss = tf.nn.softmax_cross_entropy_with_logits(labels=tf.one_hot(y, depth=NUM_CLASSES), logits=logits)
     loss = tf.reduce_mean(loss)
     target = y
 
@@ -299,8 +311,8 @@ def train(step, imgs_path=TRAIN_IMG_DIR, epochs=NUM_EPOCHS):
     # )
     # 만들어준 모델에서 back-prop 과 가중치 업데이트를 수행하기 위해 optimizer 메소드를 사용
     # 기존 텐서플로우에는 weight-decay 가 설정 가능한 optimizer 부재, Tensorflow_addons 의 SGDW 메소드 사용
-    # lr_temp = LR_INIT
-    optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=LR_INIT, weight_decay=LR_DECAY, name='optimizer')
+    lr_temp = LR_INIT
+    optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=lr_temp, weight_decay=LR_DECAY, name='optimizer')
     # optimizer = tf.optimizers.SGD(momentum=MOMENTUM, learning_rate=0.001, name='optimizer')
 
     # 파라미터(=가중치) 들을 직접 관리해야 하므로 논문 조건에 따라 초기화
@@ -311,9 +323,9 @@ def train(step, imgs_path=TRAIN_IMG_DIR, epochs=NUM_EPOCHS):
 
     # 정해진 횟수(90번)만큼 training 진행 -> 전체 트레이닝셋을 90번 반복한다는 의미
     for epoch in range(epochs):
-        # if (epoch+1) % 10 == 0 and lr_temp >= 1e-5:
-        #     lr_temp /= 10;
-        #     optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=lr_temp, weight_decay=LR_DECAY, name='optimizer')
+        if (epoch+1) % 10 == 0 and lr_temp >= 1e-5:
+            lr_temp /= 10;
+            optimizer = tfa.optimizers.SGDW(momentum=MOMENTUM, learning_rate=lr_temp, weight_decay=LR_DECAY, name='optimizer')
         print('epoch {}'.format(epoch+1))
         # 몇 번째 batch 수행 중인지 확인 위한 변수
         foo = 1
@@ -331,6 +343,7 @@ def train(step, imgs_path=TRAIN_IMG_DIR, epochs=NUM_EPOCHS):
             imgs, lbls = flip_image(imgs, lbls)
             imgs, lbls = crop_image(imgs, lbls)
             imgs, lbls = fancy_pca(imgs, lbls)
+            imgs = minmax(imgs)
             train_X, train_Y = make_dataset(imgs, lbls)
 
             # batch_size(128)로 나뉘어진 데이터에서 트레이닝 수행, e.g., 2000개의 데이터 / 128 = 15.625 -> 16개의 batch

realtime_task.py
@@ -11,6 +11,8 @@ import numpy as np
 import cv2
 
 # Data directory
+from sklearn.preprocessing import minmax_scale
+
 INPUT_ROOT_DIR = './input/task'
 TEST_IMG_DIR = os.path.join(INPUT_ROOT_DIR, 'test')
 OUTPUT_ROOT_DIR = './output/task'
@@ -96,18 +98,39 @@ def load_param(ckpts_path=OUTPUT_ROOT_DIR):
 def test(image, loaded_param, dirs):
     img = resize_image(image)
 
-    pred = prediction(tf.cast(tf.reshape(img, [-1,227,227,3]), dtype=tf.float32), loaded_param['arr_0'])
+    pred = prediction(tf.cast(tf.reshape(img, [-1, 227, 227, 3]), dtype=tf.float32), loaded_param['arr_0'])
 
     return dirs[pred[0]]
 
+def minmax(images):
+    # R, G, B 채널을 각각 순회하며 계산된 값을 각 픽셀마다 가감
+    scaled_img = np.array(img).copy()
+    for idx in range(3):
+        scaled_img[..., idx] = minmax_scale(img[..., idx], feature_range=(-1, 1))
+
+    return scaled_img
 
 if __name__ == "__main__":
     camera = cv2.VideoCapture(0);
     classes, param = load_param()
+    f, img = camera.read();
+    pred = test(image=img, loaded_param=param, dirs=classes)
+    # foo = [1,2,3,4,5]
+    # pred = 0
+    # bar = 0
 
     while cv2.waitKey(1) != ord('q'):
         f, img = camera.read();
-        pred = test(image=img, loaded_param=param, dirs=classes)
+        new = test(image=img, loaded_param=param, dirs=classes)
+        # new = bar
+        if pred != new:
+            cv2.destroyWindow(pred)
+            # cv2.destroyWindow('{}'.format(foo[pred]))
+            pred = new
+
         cv2.imshow(pred, img);
+        # cv2.imshow('{}'.format(foo[pred]), img)
+        # bar += 1
+
     camera.release()
     cv2.destroyAllWindows()

test.py
@@ -26,7 +26,7 @@ MOMENTUM = 0.9
 LR_DECAY = 0.0005         # == weight_decay
 LR_INIT = 0.01
 NUM_CLASSES = 3
-IMAGENET_MEAN = np.array([104., 117., 124.], dtype=np.float)
+# IMAGENET_MEAN = np.array([104., 117., 124.], dtype=np.float)
 
 # Data directory
 INPUT_ROOT_DIR = './input/task'
@@ -80,15 +80,28 @@ def resize_images(imgpaths):
     return images
 
 
+def minmax(images):
+    scaled_images = list()
+    for img in images:
+        # R, G, B 채널을 각각 순회하며 계산된 값을 각 픽셀마다 가감
+        scaled_img = np.array(img).copy()
+        for idx in range(3):
+            scaled_img[..., idx] = minmax_scale(img[..., idx], feature_range=(-1, 1))
+        scaled_images.append(scaled_img)
+
+    return scaled_images
+
+
 # RGB jittering
-def fancy_pca(images, labels, alpha_std=0.1):
+def fancy_pca(images, labels, imgmean, alpha_std=0.1):
     # print('Start Jittering')
     pca_images,pca_labels = images.copy(),labels.copy()
     for img,lbl in zip(images, labels):
         orig_img = np.array(img, dtype=np.float).copy()
         # 이미지 픽셀값에서 이미지넷 평균 픽셀값을 빼줌(평균 픽셀값은 사전에 정의됨)
         img_rs = np.reshape(img, (-1, 3))
-        img_centered = img_rs - IMAGENET_MEAN
+        # img_centered = img_rs - IMAGENET_MEAN
+        img_centered = img_rs - imgmean
         # 해당 이미지의 공분산 행렬 구함
         img_cov = np.cov(img_centered, rowvar=False)
         # 고유벡터, 고유값 구함
@@ -107,7 +120,8 @@ def fancy_pca(images, labels, alpha_std=0.1):
         # R, G, B 채널을 각각 순회하며 계산된 값을 각 픽셀마다 가감
         for idx in range(3):
             orig_img[..., idx] += add_vect[idx]
-            minmax_scale(orig_img[..., idx], feature_range=(0., 1.), copy=False)
+            # orig_img[..., idx] = minmax_scale(np.array(orig_img[..., idx]), feature_range=(0, 255))
+            # minmax_scale(np.array(orig_img[..., idx], dtype=np.uint16), feature_range=(0, 255), copy=False)
         # 0~255(rgb픽셀값) 범위로 값 재설정
         pca_img = orig_img
         pca_images.append(pca_img)
@@ -241,22 +255,44 @@ def test(imgs_path=TEST_IMG_DIR, ckpts_path=OUTPUT_ROOT_DIR):
     cv2.destroyAllWindows()
 
     images = resize_images(filepaths)
+    print(images[-1])
+    print(type(images[-1]))
+
+    imgmean = list()
+    imgmean.append(np.mean(images[:][:][:][0]))
+    imgmean.append(np.mean(images[:][:][:][1]))
+    imgmean.append(np.mean(images[:][:][:][2]))
+
     cv2.imshow('resize', np.array(images[-1]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
     images, labels = flip_image(images, labels)
+    print(images[-1])
+    print(type(images[-1]))
     cv2.imshow('flip', np.array(images[-1]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
     images, labels = crop_image(images, labels)
+    print(images[-1])
+    print(type(images[-1]))
     cv2.imshow('crop', np.array(images[-1]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    images, labels = fancy_pca(images, labels)
+
+    images, labels = fancy_pca(images, labels, imgmean)
+    print(images[-1])
+    print(type(images[-1]))
     cv2.imshow('pca', np.array(images[-1]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    images = minmax(images)
+
     test_X, test_Y = make_dataset(images, labels)
+    print(images[-1])
+    print(type(images[-1]))
     cv2.imshow('dataset', np.array(test_X[-1]))
     cv2.waitKey(0)
     cv2.destroyAllWindows()